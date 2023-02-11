4340b1a2de16086ffdf0a4871e1c4b8676e706cb
David Inga <davidinga@gmail.com>
2023-02-11 22:15:35 +0000

    Added problem statement.

--- main.swift
+++ main.swift
@@ -1,18 +1,43 @@
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+Constraints:
+
+0 <= s.length <= 5 * 10^4
+s consists of English letters, digits, symbols and spaces.
+*/
+
 func lengthOfLongestSubstring(_ s: String) -> Int {
+    var maxLength = 0
     let s = s.map(Character.init)
-    var maxChars = 0
-    var chars: [Character: Int] = [:]
-    var l = 0, r = 0
-    while r < s.count {
-        if let i = chars[s[r]] {
-            repeat {
-                chars[s[l]] = nil
-                l += 1
-            } while l <= i
+    var chars: Set<Character> = []
+    var leftIdx = 0
+    for rightIdx in s.indices {
+        while chars.contains(s[rightIdx]) {
+            chars.remove(s[leftIdx])
+            leftIdx += 1
         }
-        maxChars = max(chars.count + 1, maxChars)
-        chars[s[r]] = r
-        r += 1
+        chars.insert(s[rightIdx])
+        maxLength = max(maxLength, rightIdx - leftIdx + 1)
     }
-    return maxChars
+    return maxLength
 }
